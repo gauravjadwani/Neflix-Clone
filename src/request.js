4fb8a58467fd640e6baadbab29f8caf49ef19109
DESKTOP-KJV6OLI\Roohdar <gaurav.jadwani@hotmail.com>
2021-05-15 16:06:09 +0530

    layout changed

--- src/request.js
+++ src/request.js
@@ -3,7 +3,13 @@ const APIKEY = "9231db9a28420c80df01d3b88b3db4e7"
 
 const request ={
     fetchTrending :`/trending/all/week?api_key=${APIKEY}&language=en-US`,
-    fetchNetflixOriginals:`/discover/tv?api_key=${APIKEY}`
+    fetchNetflixOriginals:`/discover/tv?api_key=${APIKEY}&with_networks=213`,
+    fetchTopRated:`/movie/top_rated?api_key=${APIKEY}&language=en-US`,
+    fetchActionMovies:`/discover/movie?api_key=${APIKEY}&with_genres=28`,
+    fetchComedyMovies:`/discover/movie?api_key=${APIKEY}&with_genres=35`,
+    fetchHorrorMovies:`/discover/movie?api_key=${APIKEY}&with_genres=27`,
+    fetchRomanceMovies:`/discover/movie?api_key=${APIKEY}&with_genres=10749`,
+    fetchDocumentariesMovies:`/discover/movie?api_key=${APIKEY}&with_genres=99`,
 }
 
 
