e220cd07d88318f79e5440bbc57fb3e6e6fbfe47
DESKTOP-KJV6OLI\Roohdar <gaurav.jadwani@hotmail.com>
2021-05-17 01:28:13 +0530

    Banner added.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import logo from './logo.svg';
 import './custom.css';
 import Row from './Row';
+import Banner from './Banner'
 import request from './request'
+import React,{setMovies, useState, useEffect} from 'react'
+import DataContext from './dataContext'
+import axios from "./axios"
 function App() {
+  const [movies, setMovies] = useState([])
+
+
+  useEffect(() => {
+    // effect
+
+    axios.get(request.fetchNetflixOriginals).then(function(val){
+      // console.log("this is val",val)
+      const random =  (Math.floor(Math.random() * (val.data.results.length -1)));
+      console.log('random ',random)
+      setMovies(val.data.results[random])
+
+    })
+
+}, [])
   return (
     <div className="App">
-      Netflix clone
-      <Row title="Top Netflix Originals" fetchUrl ={request.fetchNetflixOriginals} isLargeRow/>
+            Netflix clone
+      <DataContext.Provider value={movies}>
+        <Banner/>
+      </DataContext.Provider>
+
+      <Row title="Top Netflix Originals" fetchUrl ={request.fetchNetflixOriginals} isLargeRow data/>
       <Row title="Top Trending" fetchUrl ={request.fetchTrending}/>
 
       <Row title="Top Rated" fetchUrl ={request.fetchTopRated}/>
