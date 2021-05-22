c3196fb33ae86e91b177c8832452f613464bb44d
DESKTOP-KJV6OLI\Roohdar <gaurav.jadwani@hotmail.com>
2021-05-23 00:23:23 +0530

    dev completed.

--- src/App.js
+++ src/App.js
@@ -6,8 +6,28 @@ import request from './request'
 import React,{setMovies, useState, useEffect} from 'react'
 import DataContext from './dataContext'
 import axios from "./axios"
+import movieTrailer from 'movie-trailer'
+import YouTube from 'react-youtube';
+import "react-responsive-modal/styles.css";
+import { Modal } from "react-responsive-modal";
+
+import ModalContext from './movieContext'
+
+
+const opts = {
+  height: '390',
+  width: '100%',
+  playerVars: {
+    // https://developers.google.com/youtube/player_parameters
+    autoplay: 1,
+  },
+};
+
+// const ModalContext = React.createContext();
 function App() {
   const [movies, setMovies] = useState([])
+  const [modalOpen, setModalOpen] = useState(false)
+  const [modalMovie, setModalMovie] = useState(null)
 
 
   useEffect(() => {
@@ -18,21 +38,28 @@ function App() {
       const random =  (Math.floor(Math.random() * (val.data.results.length -1)));
       console.log('random ',random)
       setMovies(val.data.results[random])
+      movieTrailer( 'Oceans Eleven' )
+        .then( response => console.log( 'response',response ) )
 
     })
 
 }, [])
+console.log('App.js')
   return (
     <div className="App">
             Netflix clone
       <DataContext.Provider value={movies}>
         <Banner/>
       </DataContext.Provider>
+      <ModalContext.Provider  value ={{setModalMovie,setModalOpen}} >
+        <Row title="Top Netflix Originals" fetchUrl ={request.fetchNetflixOriginals} isLargeRow data/>
+        <Row title="Top Trending" fetchUrl ={request.fetchTrending}/>
 
-      <Row title="Top Netflix Originals" fetchUrl ={request.fetchNetflixOriginals} isLargeRow data/>
-      <Row title="Top Trending" fetchUrl ={request.fetchTrending}/>
-
-      <Row title="Top Rated" fetchUrl ={request.fetchTopRated}/>
+        <Row title="Top Rated" fetchUrl ={request.fetchTopRated}/>
+      </ModalContext.Provider>
+      <Modal open={modalOpen} onClose={()=>setModalOpen(false)}>
+      <YouTube videoId={modalMovie} /> 
+        </Modal>
     </div>
   );
 }
--- src/Coloumn.js
+++ src/Coloumn.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React,{useContext} from 'react'
+
+import MovieContext from './movieContext'
+import movieTrailer from 'movie-trailer'
+
+
+async function  handleClick(e, movie,context){
+    const movieName = (movie.name)?(movie.name):(movie.title)
+    const movieTralerId =  await movieTrailer( movieName, { id:true} )
+    console.log('handleClick',movieTralerId,movieName)
+    context.setModalOpen(true)
+    context.setModalMovie(movieTralerId)
+   
+}
 
 function Column({movie, isLargeRow}){
     // console.log("mobies",movie,isLargeRow)
+    // const d = useContext(DataContext);
+    const d = useContext(MovieContext);
+    console.log('contexttt',d)
     return(
-        <div className="col">
+        <div className="col" onClick={(e)=>handleClick(e, movie,d)}>
             <img className="image" src={!isLargeRow ? `https://image.tmdb.org/t/p/original/${movie.backdrop_path}`
             : `https://image.tmdb.org/t/p/original/${movie.poster_path}`}/>
             <div className="movie-title">{(movie.name)?(movie.name):(movie.title)}</div>
