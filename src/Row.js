b156af9116116a76167014f8cc85236a4816a3cf
DESKTOP-KJV6OLI\Roohdar <gaurav.jadwani@hotmail.com>
2021-05-23 00:23:23 +0530

    dev completed.

--- src/Row.js
+++ src/Row.js
@@ -1,9 +1,26 @@
-import React, {useState,useEffect} from 'react'
+import React, {useState,useEffect,useContext} from 'react'
 import Coloumn from './Coloumn'
 import axios from "./axios"
+import YouTube from 'react-youtube';
+import DataContext from './dataContext'
 
+function _onReady(e) {
+    // access to player in all event handlers via event.target
+    // e.target.pauseVideo();
+  }
 function Row({title, fetchUrl, isLargeRow}){
     const [movies, setMovies] = useState([])
+
+    
+    const opts = {
+        height: '390',
+        width: '100%',
+        playerVars: {
+          // https://developers.google.com/youtube/player_parameters
+          autoplay: 1,
+        },
+      };
+
     useEffect(() => {
         // effect
         axios.get(fetchUrl).then(function(val){
@@ -12,6 +29,8 @@ function Row({title, fetchUrl, isLargeRow}){
         })
 
     }, [fetchUrl])
+    // const d = useContext(DataContext);
+    console.log("RowCOntext",fetchUrl)
     return(
         <>
         <div className="cat-title"><h1>{title}</h1></div>
@@ -20,7 +39,10 @@ function Row({title, fetchUrl, isLargeRow}){
             {(movies.length > 0) ? (
                movies.map((val, i)=>(<Coloumn movie={val} key={i} isLargeRow={isLargeRow}/>))
             ):(null)}
+            
         </div>
+        {/* <YouTube videoId="ImMGNQ2OEjo" opts={opts} /> */}
+
         </>
     )
 }
